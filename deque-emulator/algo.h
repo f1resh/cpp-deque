093cf90849f93c3cb37bbfc72f6117788a50b58c
Pavel Akulov <pavel.akulov.88@gmail.com>
2024-11-28 15:12:25 +0100

    Review 1

--- deque-emulator/algo.h
+++ deque-emulator/algo.h
@@ -4,17 +4,24 @@
 
 template<class T, class Comp>
 std::deque<T> Merge(const std::deque<T>& half1, const std::deque<T>& half2, const Comp& comparator) {
-    size_t i1 = 0, i2 = 0, size1 = half1.size(), size2 = half2.size();
+    size_t index1 = 0;
+	size_t index2 = 0;
+	size_t size1 = half1.size();
+	size_t size2 = half2.size();
     std::deque<T> result;
-    while (i1 < size1 && i2 < size2){
-        if (comparator(half1[i1], half2[i2])) {
-            result.push_back(half1[i1++]);
+    while (index1 < size1 && index2 < size2){
+        if (comparator(half1[index1], half2[index2])) {
+            result.push_back(half1[index1++]);
         } else {
-            result.push_back(half2[i2++]);
+            result.push_back(half2[index2++]);
         }
     }
-    while (i2 < size2) result.push_back(half2[i2++]);
-    while (i1 < size1) result.push_back(half1[i1++]);
+    while (index2 < size2){
+		result.push_back(half2[index2++]);
+	}
+    while (index1 < size1){
+		result.push_back(half1[index1++]);
+	}
     return result;
 }
 
@@ -25,6 +32,6 @@ std::deque<T> MergeSort(const std::deque<T>& src, const Comp& comparator) {
     }
     const size_t mid = src.size() / 2;
     const std::deque<T> left_sorted = MergeSort(std::deque<T>(src.begin(), src.begin() + mid), comparator);
-    const std::deque<T> right_sorted = MergeSort(std::deque<T>(src.begin() + mid, src.end()) ,comparator);
+    const std::deque<T> right_sorted = MergeSort(std::deque<T>(src.begin() + mid, src.end()), comparator);
     return Merge(left_sorted, right_sorted, comparator);
 }
